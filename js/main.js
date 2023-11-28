ab3106ce8aa3944ff311247c73f77b0f51dcd201
dieromeo <diegoromeo99@icloud.com>
2023-11-28 17:31:11 +0100

    feat: milestone 3

--- js/main.js
+++ js/main.js
@@ -10,12 +10,20 @@ createApp({
                 { text: 'secondo todo', done: true },
                 { text: 'terzo todo', done: false },
                 { text: 'quarto todo', done: false },
-            ]
+            ],
+
+            inputText: '',
         };
     },
     methods: {
         removeTodo(index) {
             this.todos.splice(index, 1);
+        },
+        addTodo() {
+            if (this.inputText.trim() !== '') {
+                this.todos.push({ text: this.inputText, done: false });
+                this.inputText = '';
+            }
         }
     }
 }).mount('#app');
