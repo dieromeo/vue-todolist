2abffca26dfd71a4e5f7a16eb04289574bcad50f
dieromeo <diegoromeo99@icloud.com>
2023-11-28 15:06:42 +0100

    feat: aggiunta todo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            todos: [
+                { text: 'primo todo', done: false },
+                { text: 'secondo todo', done: false },
+                { text: 'terzo todo', done: false },
+                { text: 'quarto todo', done: false },
+            ]
+        };
+    },
+}).mount('#app');
+
